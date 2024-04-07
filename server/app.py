e05d3f1021f1fc4624d28215ad5fe611372dda71
Wanjiru Mburu <wanjirumarionne@gmail.com>
2024-04-08 00:56:31 +0300

    Completed Building a GET API Lab

--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
+from sqlalchemy import desc
 
 from models import db, Bakery, BakedGood
 
@@ -18,21 +19,32 @@ db.init_app(app)
 def index():
     return '<h1>Bakery GET API</h1>'
 
-@app.route('/bakeries')
-def bakeries():
-    return ''
+@app.route("/bakeries")
+def all_bakeries():
+    bakeries_dict = [bakery.to_dict(rules=("-baked_goods",)) for bakery in Bakery.query.all()]
 
-@app.route('/bakeries/<int:id>')
+    return make_response(bakeries_dict, 200)
+
+@app.route("/bakeries/<int:id>")
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter_by(id=id).first()
 
-@app.route('/baked_goods/by_price')
+    if bakery:
+        return make_response(bakery.to_dict(), 200)
+    else:
+        return make_response({"errors": "Bakery not found"}, 404)
+    
+@app.route("/baked_goods/by_price")
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(desc("price")).all()
+    baked_goods_dict = [baked_good.to_dict(rules=("-bakery",)) for baked_good in baked_goods]
 
-@app.route('/baked_goods/most_expensive')
+    return make_response(baked_goods_dict, 200)
+
+@app.route("/baked_goods/most_expensive")
 def most_expensive_baked_good():
-    return ''
+    most_expensive_baked_good = BakedGood.query.order_by(desc("price")).limit(1).first()
+    return make_response(most_expensive_baked_good.to_dict(), 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
